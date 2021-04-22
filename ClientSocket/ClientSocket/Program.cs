88e331a3a98a8879757c128c6d43ca658536187b
kjr04205 <63985698+kjr04205@users.noreply.github.com>
2021-04-22 16:53:53 +0900

    Add files via upload

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ClientSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect("192.168.0.19", 53188);
+
+            if (socket.Connected)
+            {
+                Console.WriteLine("서버에 연결 되었습니다.");
+            }
+
+            string message = string.Empty;
+            
+            while((message = Console.ReadLine()) != "x")
+            {
+                byte[] buff = Encoding.UTF8.GetBytes(message);
+                socket.Send(buff);
+            }
+        }
+    }
+}
